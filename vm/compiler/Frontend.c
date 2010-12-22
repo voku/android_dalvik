7b503165dcfdd3365077c537e1f12e96715f5bc5
Ben Cheng <bccheng@android.com>
2010-12-23 04:27:41 +0800

    Eliminate the chaining cells for direct JNI calls.
    The JNI code is dispatched through TEMPLATE_INVOKE_METHOD_NATIVE, so
    having a chaining cell for a non-virtual native call simply wastes
    space.
    
    Change-Id: Iaf944f745a0723a7772ea168fb35f79e79b21fae

--- vm/compiler/Frontend.c
+++ vm/compiler/Frontend.c
@@ -632,13 +632,16 @@ bool dvmCompileTrace(JitTraceDescription *desc, int numMaxInsts,
         if (curBB->taken == NULL &&
             (isGoto(lastInsn) || isInvoke ||
             (targetOffset != UNKNOWN_TARGET && targetOffset != curOffset))) {
-            BasicBlock *newBB;
+            BasicBlock *newBB = NULL;
             if (isInvoke) {
                 /* Monomorphic callee */
                 if (callee) {
-                    newBB = dvmCompilerNewBB(kChainingCellInvokeSingleton);
-                    newBB->startOffset = 0;
-                    newBB->containingMethod = callee;
+                    /* JNI call doesn't need a chaining cell */
+                    if (!dvmIsNativeMethod(callee)) {
+                        newBB = dvmCompilerNewBB(kChainingCellInvokeSingleton);
+                        newBB->startOffset = 0;
+                        newBB->containingMethod = callee;
+                    }
                 /* Will resolve at runtime */
                 } else {
                     newBB = dvmCompilerNewBB(kChainingCellInvokePredicted);
@@ -664,10 +667,12 @@ bool dvmCompileTrace(JitTraceDescription *desc, int numMaxInsts,
                 newBB->startOffset = targetOffset;
 #endif
             }
-            newBB->id = numBlocks++;
-            curBB->taken = newBB;
-            lastBB->next = newBB;
-            lastBB = newBB;
+            if (newBB) {
+                newBB->id = numBlocks++;
+                curBB->taken = newBB;
+                lastBB->next = newBB;
+                lastBB = newBB;
+            }
         }
     }
 
