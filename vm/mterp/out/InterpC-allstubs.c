2ad989b576561461f01e42eb0a8f4ef4f64a5cc6
Andy McFadden <fadden@android.com>
2011-02-15 06:04:42 +0800

    Fix "all stubs" interpreter
    The "all stubs" interpreter is the recommended starting point for
    anyone creating a "fast" interpreter on a new CPU.  This cleans
    up some minor bit rot in the code and documentation.
    
    Bug 3452689
    
    Change-Id: I6bc3b0b5da11955d842d5fc5f16cb1a2209d4a89

--- vm/mterp/out/InterpC-allstubs.c
+++ vm/mterp/out/InterpC-allstubs.c
@@ -3056,9 +3056,8 @@ bool dvmMterpStdRun(MterpGlue* glue)
      */
     changeInterp = setjmp(jmpBuf) -1;
     if (changeInterp >= 0) {
-        Thread* threadSelf = dvmThreadSelf();
         LOGVV("mterp threadid=%d returning %d\n",
-            threadSelf->threadId, changeInterp);
+            dvmThreadSelf()->threadId, changeInterp);
         return changeInterp;
     }
 
@@ -3090,11 +3089,13 @@ bool dvmMterpStdRun(MterpGlue* glue)
 
         u2 inst = /*glue->*/pc[0];
         Handler handler = (Handler) gDvmMterpHandlers[inst & 0xff];
+        (void) gDvmMterpHandlerNames;   /* avoid gcc "defined but not used" */
         LOGVV("handler %p %s\n",
             handler, (const char*) gDvmMterpHandlerNames[inst & 0xff]);
         (*handler)(glue);
     }
 }
+
 
 /*
  * C mterp exit point.  Call here to bail out of the interpreter.
