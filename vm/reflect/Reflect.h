38e96e636822c6a246076a928368d5b978bd8ea9
Jesse Wilson <jessewilson@google.com>
2011-03-29 05:26:34 +0800

    Support reflective lookup of single fields.
    Change-Id: Ie7058fa9de8212b0f52d5d9b0ec7d46242a851fc
    http://b/4087759

--- vm/reflect/Reflect.h
+++ vm/reflect/Reflect.h
@@ -37,6 +37,11 @@ bool dvmValidateBoxClasses();
 ArrayObject* dvmGetDeclaredFields(ClassObject* clazz, bool publicOnly);
 
 /*
+ * Get the named field.
+ */
+Object* dvmGetDeclaredField(ClassObject* clazz, StringObject* nameObj);
+
+/*
  * Get all constructors declared by a class.
  */
 ArrayObject* dvmGetDeclaredConstructors(ClassObject* clazz, bool publicOnly);
