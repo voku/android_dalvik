1111e456f935adb11604d5dc05939a0b948d8077
Lars Moelleken <voku@FUCKUP.(none)>
2011-07-26 16:33:22 +0200

    + settings for Spica (need testing)

--- vm/Dvm.mk
+++ vm/Dvm.mk
@@ -270,7 +270,7 @@ MTERP_ARCH_KNOWN := false
 
 ifeq ($(dvm_arch),arm)
   #dvm_arch_variant := armv7-a
-  #LOCAL_CFLAGS += -march=armv7-a -mfloat-abi=softfp -mfpu=vfp
+  LOCAL_CFLAGS += -march=armv6zk -mfloat-abi=softfp -mfpu=vfp
   MTERP_ARCH_KNOWN := true
   # Select architecture-specific sources (armv4t, armv5te etc.)
   LOCAL_SRC_FILES += \
